eb62b51fcc69cb944c5abbb10b7e6b31c3098558
bzl <leonardzbotha@gmail.com>
2014-10-04 14:03:33 +0200

    added class for neuron layers

---
+++ neuron_layer.h
@@ -0,0 +1,16 @@
+#ifndef neuron_layer_h
+#define neuron_layer_h
+
+#include <vector>
+
+#include "neuron.h"
+
+class neuron_layer{
+    private:
+        std:::vecotor<neuron> neurons;
+
+    public:
+
+};
+
+#endif
