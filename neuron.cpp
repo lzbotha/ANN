4aee33cc2a052f66dc1f8b400f18f4ab45d6022a
bzl <leonardzbotha@gmail.com>
2014-10-04 13:57:10 +0200

    added accessors and mutators to neuron

--- neuron.cpp
+++ neuron.cpp
@@ -8,14 +8,14 @@ neuron::neuron(int num_inputs) : weights_from_input(std::vector<float>(num_input
     std::srand(std::time(0)); // use current time as seed for random generator
 
     // Initialize all input weights to random values in the interval [-0.5, 0,5]
-    std::transform(weights_from_input.begin(), weights_from_input.end(), weights_from_input.begin(), [](float weight){
+    std::transform(weights_from_input.begin(), weights_from_input.end(), weights_from_input.begin(), [](float & weight){
         return (((float)rand()) / (float)RAND_MAX) - 0.5f;
     });
 }
 
 void neuron::print_weights(){
     // print each weight to stdout
-    for_each(weights_from_input.begin(), weights_from_input.end(), [](float weight){
+    for_each(weights_from_input.begin(), weights_from_input.end(), [](float & weight){
         std::cout << weight << std::endl;
     });
 }
--- neuron.h
+++ neuron.h
@@ -3,7 +3,7 @@
 #include <vector>
 
 class neuron{
-    public:
+    private:
         /**
          The value stored at this neuron
         */
@@ -14,8 +14,12 @@ class neuron{
         */
         std::vector<float> weights_from_input;
 
+    public:
+        
         neuron(int num_inputs);
         void print_weights(void);
+        void set_value(float value) {this->value = value;}
+        float get_value(void) {return this->value;}
 };
 
 #endif
