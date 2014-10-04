2afeed392e1fc84a109de699e63fa7f8d8d8d7f5
bzl <leonardzbotha@gmail.com>
2014-10-04 14:10:59 +0200

    constructor coded

--- neuron_layer.cpp
+++ neuron_layer.cpp
@@ -1,5 +1,7 @@
 #include "neuron_layer.h"
 
-neuron_layer::neuron_layer(int size_of_layer, int size_of_prev_layer = 0){
+neuron_layer::neuron_layer(int size_of_layer, int size_of_prev_layer = 0) :
+    neurons(std::vector<neuron>(size_of_layer, neuron(size_of_prev_layer)))
+{
 
 }
