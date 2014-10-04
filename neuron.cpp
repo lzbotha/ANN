24dadf265a85e3d6c2ba5b029c6ebf1f0fa29195
bzl <leonardzbotha@gmail.com>
2014-10-04 12:37:43 +0200

    added code to print all weights at a neuron to stdout

--- neuron.cpp
+++ neuron.cpp
@@ -1,5 +1,6 @@
 #include <algorithm>
 #include <ctime>
+#include <iostream>
 
 #include "neuron.h"
 
@@ -9,5 +10,12 @@ neuron::neuron(int num_inputs) : weights_from_input(std::vector<float>(num_input
     // Initialize all input weights to random values in the interval [-0.5, 0,5]
     std::transform(weights_from_input.begin(), weights_from_input.end(), weights_from_input.begin(), [](float weight){
         return (((float)rand()) / (float)RAND_MAX) - 0.5f;
+    });
+}
+
+void neuron::print_weights(){
+
+    for_each(weights_from_input.begin(), weights_from_input.end(), [](float weight){
+        std::cout << weight << std::endl;
     });
 }
